ce6ad0905790d89d951cfdf5dfb6212c4b97eb93
Sam Chen <sam.chen@geospock.com>
2020-05-04 20:25:25 +0100

    Committing an example serialised weapon to consider data modelling.

--- src/main/kotlin/io/hsar/wh40k/combatsimulator/model/unit/Attribute.kt
+++ src/main/kotlin/io/hsar/wh40k/combatsimulator/model/unit/Attribute.kt
@@ -13,6 +13,9 @@ enum class Attribute {
     DAMAGE_REDUCTION_LEG_L,
     DAMAGE_REDUCTION_LEG_R,
     DAMAGE_OUTPUT,
+    DAMAGE_TYPES,
+    WEAPON_TYPE,
+    ATTACK_TYPES,
     IN_MELEE_COMBAT
 }
 
@@ -21,6 +24,50 @@ enum class Attribute {
  */
 sealed class AttributeValue
 
-data class NumericValue(val value: Int): AttributeValue() {
-    operator fun plus(other: NumericValue): NumericValue = NumericValue(this.value + other.value)
+data class NumericValue(val value: Int) : AttributeValue() {
+    operator fun plus(other: NumericValue): NumericValue {
+        return (this.value + other.value)
+                .let { newValue ->
+                    NumericValue(newValue)
+                }
+    }
+}
+
+enum class WeaponType {
+    MELEE,
+    PISTOL,
+    BASIC,
+    HEAVY
+    // #TODO Can optimise this: https://stackoverflow.com/a/19277247/2756877
+}
+
+data class WeaponTypeValue(val value: WeaponType): AttributeValue() {
+    operator fun plus(other: WeaponTypeValue): WeaponTypeValue {
+        return Math.max(this.value.ordinal, other.value.ordinal)
+                .let { largerOrdinal ->
+                    WeaponType.values()[largerOrdinal]
+                }
+                .let { newValue ->
+                    WeaponTypeValue(newValue)
+                }
+    }
+}
+
+sealed class AttackType(val damage: String, val numberOfAttacks: Int) {
+    class MELEE(damage: String): AttackType(damage, 1)
+    class RANGED_SINGLE(damage: String): AttackType(damage, 1)
+    class RANGED_SEMI_AUTO_BURST(damage: String,shotsFired: Int): AttackType(damage, shotsFired)
+    class RANGED_FULL_AUTO_BURST(damage: String, shotsFired: Int): AttackType(damage, shotsFired)
+}
+
+/**
+ * This attribute value type stacks, i.e. additional attributes are added to the end of the list.
+ */
+data class StackingValue<T>(val value: List<T>): AttributeValue() {
+    operator fun plus(other: StackingValue<T>): StackingValue<T> {
+        return (this.value + other.value)
+                .let { newValue ->
+                    StackingValue(newValue)
+                }
+    }
 }
--- src/main/kotlin/io/hsar/wh40k/combatsimulator/model/unit/Equipment.kt
+++ src/main/kotlin/io/hsar/wh40k/combatsimulator/model/unit/Equipment.kt
@@ -19,12 +19,10 @@ data class SimpleEquipmentInfo(
 ) : EquipmentInfo()
 
 /**
- * Reoresents weapons that have their own turn by turn state (ammo, fire selector, jammed, misfire etc.)
+ * Represents weapons that have their own turn by turn state (ammo, fire selector, jammed, misfire etc.)
  */
 data class WeaponInfo(
         val itemRef: String,
-        override val modifiesAttributes: Map<Attribute, AttributeValue>,
-        val maxAmmo: Int,
-        val currAmmo: Int
-        // #TODO: Fire selector?
+        override val modifiesAttributes: Map<Attribute, AttributeValue>
+        // #TODO: How does this deserialize from file?
 ) : EquipmentInfo()
