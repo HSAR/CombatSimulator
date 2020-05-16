f882950e22def939eabe3750b80293c641d04e53
Sam Chen <sam.chen@geospock.com>
2020-05-16 20:24:29 +0100

    Deployed dark Mockito magics to fix mocking errors.

--- src/test/kotlin/io/hsar/wh40k/combatsimulator/model/WorldTest.kt
+++ src/test/kotlin/io/hsar/wh40k/combatsimulator/model/WorldTest.kt
@@ -1,16 +1,15 @@
 package io.hsar.wh40k.combatsimulator.model
 
 import TestUtils
-import io.hsar.wh40k.combatsimulator.logic.*
-import io.hsar.wh40k.combatsimulator.model.AttackExecution.calcDamage
-import io.hsar.wh40k.combatsimulator.model.AttackExecution.rollHits
-import io.hsar.wh40k.combatsimulator.random.RandomDice
+import io.hsar.wh40k.combatsimulator.logic.AimAction
+import io.hsar.wh40k.combatsimulator.logic.HalfAim
+import io.hsar.wh40k.combatsimulator.logic.HalfMove
+import io.hsar.wh40k.combatsimulator.logic.SingleRangedAttack
+import io.hsar.wh40k.combatsimulator.logic.TargetedAction
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
-import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
-import org.mockito.Mockito.any
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
 
@@ -18,20 +17,20 @@ class WorldTest {
 
     @Test
     fun testExecuteActions() {
+        // Arrange
         val world = TestUtils.getGenericTwoUnitWorld(MapPosition(1, 6), MapPosition(5, 4))
-        val aimAcion = AimAction(HalfAim)
+        val aimAction = AimAction(HalfAim)
         val singleRangedAttack = SingleRangedAttack(30, "1d10+4")
         val targetedAction = TargetedAction(singleRangedAttack, world.enemyForces[0])
-        val actionsToExecute = listOf<TurnAction>(
-                aimAcion, targetedAction
+        val actionsToExecute = listOf(
+                aimAction, targetedAction
         )
-        val mockAttackExecution = mock(AttackExecution.javaClass)
-        Mockito.`when`(mockAttackExecution.rollHits(world.friendlyForces[0], world.enemyForces[0], singleRangedAttack)).thenAnswer {
-            return@thenAnswer 1
-        }
-        Mockito.`when`(mockAttackExecution.calcDamage(world.friendlyForces[0], world.enemyForces[0], singleRangedAttack)).thenAnswer {
-            return@thenAnswer 5
-        }
+
+        val mockAttackExecution = mock(AttackExecution::class.java)
+        `when`(mockAttackExecution.rollHits(world.friendlyForces[0], world.enemyForces[0], singleRangedAttack)).thenReturn(1)
+        `when`(mockAttackExecution.calcDamage(world.friendlyForces[0], world.enemyForces[0], singleRangedAttack)).thenReturn(5)
+
+        // Act
         world.executeActions(world.friendlyForces[0], actionsToExecute)
     }
 
