53221eec783ab086737cb1752efd9201eba1e87d
Sam Chen <sam.chen@geospock.com>
2020-06-11 22:13:56 +0100

    Misc tidying up. Added full stop and new line when aiming Added playerWolfe unit to starting lineup in party

--- src/main/kotlin/io/hsar/wh40k/combatsimulator/model/UnitInstance.kt
+++ src/main/kotlin/io/hsar/wh40k/combatsimulator/model/UnitInstance.kt
@@ -108,6 +108,7 @@ class UnitInstance(
                                 }
                                 else -> TODO()
                             }
+                            println(".")
                         }
                     }
                 }
