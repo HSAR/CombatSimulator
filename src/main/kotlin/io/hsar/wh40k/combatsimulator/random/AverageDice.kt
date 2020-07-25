ab54341e087cebf2f64f3aeae51be21a511eb61c
markyMitch <mark.mitchell700@gmail.com>
2020-07-25 22:51:44 +0100

    removed unused method in averageDice

--- src/main/kotlin/io/hsar/wh40k/combatsimulator/random/AverageDice.kt
+++ src/main/kotlin/io/hsar/wh40k/combatsimulator/random/AverageDice.kt
@@ -9,8 +9,5 @@ object AverageDice {
     private val roller = DiceRoller(AverageNumberGenerator())
 
     fun roll(diceString: String): Int = parser.parse(diceString, roller).totalRoll
-
-    fun averageDegrees(skillTarget: Int) {
-
-    }
+    
 }
